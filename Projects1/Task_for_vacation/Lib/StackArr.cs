0687df2c1d0151b83e2d0bb02dce813377e71806
IlyaDesyatkov <ilyades95@gmail.com>
2016-02-13 04:03:12 +0400

    Add

--- Projects1/Task_for_vacation/Lib/StackArr.cs
+++ Projects1/Task_for_vacation/Lib/StackArr.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lib
 {
-    public class StackArr<T> : IStack<T>
+    public class StackArr<T> : IStack<T>, IEnumerable<T>
     {
         private T[] array;
         private int top; 
@@ -44,6 +44,16 @@ namespace Lib
         public int GetCount
         {
             get { return top; }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = top - 1; i >= 0; i--)
+                yield return array[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
