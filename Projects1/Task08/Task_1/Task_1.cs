8f54a4acb91f2008dde0473e187a12bc583ad98d
IlyaDesyatkov <ilyades95@gmail.com>
2016-02-18 01:43:12 +0400

    Add Task08

---
+++ Projects1/Task08/Task_1/Task_1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Task_1
+    {
+        public static void Main()
+        {
+            int[] arr = new int[100];
+            int i = 0;
+            string str;
+            int j = 0;
+            Console.WriteLine("Вводите массив целых чисел, по окончанию введите 'end':");
+            do
+            {
+                str = Console.ReadLine();
+                if (str == "end") break;
+                arr[i] = Convert.ToInt32(str);
+                ++i; ++j;
+            } while (true);
+            Console.WriteLine("Массив");
+            foreach (var t in arr)
+            {
+                Console.Write(t + " ");
+            }
+            Console.WriteLine("\nСумма массива: {0}", arr.PlusAll());
+
+            Console.ReadLine();    
+        }
+    }
+    public static class Plus
+    {
+        public static int PlusAll(this IEnumerable<int> collection)
+        {
+            var items = new List<int>();
+            int s = 0;
+            foreach (var t in collection)
+            {
+                s += t;
+            }
+            return s;
+        }
+    }
+}
---
+++ Projects1/Task08/Task_2/Task_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Task_2
+    {
+        public static void Main()
+        {
+             Console.WriteLine("Определим, число ли ваша строка?");
+             Console.WriteLine("Вводите вашу строку:");
+             string str = Console.ReadLine();
+             char[] ch = str.ToCharArray();
+             var a = from i in ch
+                     where char.IsDigit()
+                     select i;
+             char[] array = a.ToArray();
+             if (a.Length == array.Length)
+             {
+                 Console.WriteLine("Строка является числом");
+             }
+             else Console.WriteLine("Строка не являетс числом");
+
+             Console.ReadKey(); 
+
+        }
+    }
+}
